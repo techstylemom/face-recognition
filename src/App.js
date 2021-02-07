be6a05f6821dd9d1d3db0313a1afaff131257cc7
irish <techstylemom@gmail.com>
2021-02-08 00:10:45 +0300

    update url

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://peaceful-lake-58397.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -107,7 +107,7 @@ class App extends Component {
         .then(response => response.json())
         .then(response =>{
           if (response) {
-            fetch('http://localhost:3000/image', {
+            fetch('https://peaceful-lake-58397.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,7 +22,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://peaceful-lake-58397.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
